745da843bfbd84566ebb93f60b616c6accad6e16
Shivam <shivamnegi840@gmail.com>
2022-08-15 16:16:28 +0530

    Removed alchemy api key

--- src/test.js
+++ src/test.js
@@ -1,7 +1,7 @@
 const {ethers} = require('ethers')
 const paypal = require('./paypal/paypal.json')
 
-const provider = new ethers.providers.JsonRpcProvider("https://eth-rinkeby.alchemyapi.io/v2/jAENTthf_i6RehJXHXY6A84cRwCrmZzX")
+const provider = new ethers.providers.JsonRpcProvider("");
 
 // Contracts 
 const paypalContract = new ethers.Contract('0x6170b96101557cc11F076AA3907f7FF87Db54EE7', paypal.output.abi, provider);
